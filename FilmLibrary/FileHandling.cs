99318baef6fbb32511231f282613df15775566ba
makakoocelot <makakoocelot@gmail.com>
2019-11-27 14:44:54 +0100

    Create new namespace

---
+++ FilmLibrary/FileHandling.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FilmLibrary
+{
+    public class FileHandling
+    {
+        public static Dictionary<string, Dictionary<string, string>> ReadFromFile(string FileName)
+        {
+
+            Dictionary<string, Dictionary<string, string>> allFilm = new Dictionary<string, Dictionary<string, string>>();
+            Dictionary<string, string> oneField = new Dictionary<string, string>();
+            string keyword = "";
+            var file = File.ReadAllLines(FileName);
+
+            for (int i = 0; i < file.Count(); i++)
+            {
+                if (file[i][0] == '[')
+                    keyword = file[i];
+                if (file[i].Contains('='))
+                {
+                    string[] result = file[i].Split('=');
+                    oneField.Add(result[0], result[1]);
+
+                }
+                if (i < file.Count() - 2 && file[i + 1][0] == '[' || i == file.Count() - 1)
+                {
+                    allFilm.Add(keyword, oneField);
+                    keyword = "";
+                    oneField = new Dictionary<string, string>();
+
+
+
+                }
+            }
+            return allFilm;
+        }
+        public static void writeToFile(string fileName, Dictionary<string, Dictionary<string, string>> filmDictionaries)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                foreach (var element in filmDictionaries)
+                {
+                    sw.WriteLine(element.Key);
+                    foreach (var el in element.Value)
+                    {
+                        sw.WriteLine($"{el.Key}={el.Value}");
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ FilmLibrary/filmHandle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmLibrary
+{
+    public class filmHandle
+    {
+        public static string filmsByTitle()
+        {
+            StringBuilder sr = new StringBuilder();
+            var filmDictionaryes = FileHandling.ReadFromFile("../../testFilms.ini");
+            Console.Write("Give me the film Title: ");
+            string title = Console.ReadLine();
+            if (filmDictionaryes.ContainsKey("[" + title + "]"))
+            {
+                var thefilm = filmDictionaryes["[" + title + "]"];
+                string result = $"Title = {title}\nDirector = {thefilm["director"]},\nRelease year = {thefilm["release_year"]}\nStars = {thefilm["stars"]}\nBudget = {thefilm["budget"]}\n";
+
+                return result;
+            }
+            else
+            {
+                return "This film is not in the file.";
+            }
+
+
+
+
+        }
+        public static void displayFilms()
+        {
+            var filmDictionaryes = FileHandling.ReadFromFile("../../testFilms.ini");
+
+            foreach (var title in filmDictionaryes.Keys)
+            {
+                var thefilm = filmDictionaryes[title];
+                string result = $"Title = {title}\nDirector = {thefilm["director"]}" +
+                    $",\nRelease year = {thefilm["release_year"]}\nStars = {thefilm["stars"]}\nBudget = {thefilm["budget"]}\n";
+
+                Console.WriteLine(result);
+            }
+
+        }
+        public static Dictionary<string, Dictionary<string, string>> addFilm()
+        {
+            Dictionary<string, Dictionary<string, string>> filmDictionary = FileHandling.ReadFromFile("../../testFilms.ini");
+            Dictionary<string, string> oneField = new Dictionary<string, string>();
+            string[] keywords = { "director", "release_year", "stars", "budget" };
+            Console.Write("Give me the film Title: ");
+            string title = "[" + Console.ReadLine() + "]";
+            foreach (string element in keywords)
+            {
+                Console.Write($"Give me the {element}: ");
+                oneField.Add(element, Console.ReadLine());
+            }
+            filmDictionary.Add(title, oneField);
+            return filmDictionary;
+
+
+
+        }
+        public static Dictionary<string, Dictionary<string, string>> deleteFilmByTitle()
+        {
+            var filmDictionaryes = FileHandling.ReadFromFile("../../testFilms.ini");
+            Console.Write("Give me the film Title: ");
+            string title = "[" + Console.ReadLine() + "]";
+            if (filmDictionaryes.ContainsKey(title))
+            {
+                filmDictionaryes.Remove(title);
+                return filmDictionaryes;
+            }
+            else
+            {
+                return filmDictionaryes;
+            }
+        }
+    }
+
+}
